02fdfbb64060ee674602b1fdd0d7bb3c44a91982
Max <makasyslar@gmail.com>
2023-10-09 12:10:37 +0300

    Some fixes

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,10 +2,10 @@ import Button from "../../ui/Button";
 
 const Hero = () => {
 	return (
-		<section className="relative isolate bg-[url('/images/hero-bg.jpg')] bg-cover bg-fixed bg-center px-0 before:absolute before:inset-0 before:-z-10 before:h-full before:w-full before:bg-black/30 md:p-5">
-			<div className="container flex min-h-[100dvh] flex-col items-start justify-center ">
+		<section className="relative isolate flex flex-col justify-center bg-[url('/images/hero-bg.jpg')] min-h-[100dvh] bg-cover bg-fixed bg-center px-0 before:absolute before:inset-0 before:-z-10 before:h-full before:w-full before:bg-black/30 md:p-5">
+			<div className="container flex flex-col items-start justify-center ">
 				<h1 className="mb-20 text-5xl font-thin leading-snug md:text-7xl">
-					Easy, quality, IT - <b className="font-bold">w</b>inning
+					Easy, quality, IT - <b className="font-bold text-blue">w</b>inning
 					<span className="sm:block">
 						<strong className="font-bold"> solutions</strong>
 					</span>
